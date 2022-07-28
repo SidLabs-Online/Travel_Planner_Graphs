e9e91f7d7f0df4b0e967e412ba3145d47b91d1d2
SidLabs <98277470+SidLabs-Online@users.noreply.github.com>
2022-07-28 20:36:27 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+# to make the plan of traveling around the world
+# Offer user to choose destinations
+# return cost of travel
+# Essentially use Graphs to make this one 
+# We may use this link : https://towardsdatascience.com/planning-to-travel-around-the-world-with-python-42fac1d21a6e
+# Another link to take some inspiration from : https://towardsdatascience.com/visualization-in-python-finding-routes-between-points-2d97d4881996
+
+# Happy Coding!
+
